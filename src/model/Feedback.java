3c532a67dfc4545f6e1c2269cec82f2f4413d592
Yash-1706 <gaikwad17yash@gmail.com>
2025-07-27 20:55:58 +0530

    Add Feedback and Message model classes to represent user feedback and ticket messages

--- src/model/Feedback.java
+++ src/model/Feedback.java
@@ -6,29 +6,35 @@ public class Feedback {
     private int feedbackId;
     private int ticketId;
     private int customerId;
-    private int agentId;
-    private int rating;
+    private int rating; // out of 5
     private String comments;
     private Timestamp submittedAt;
 
+    public Feedback(int feedbackId, int ticketId, int customerId, int rating, String comments, Timestamp submittedAt) {
+        this.feedbackId = feedbackId;
+        this.ticketId = ticketId;
+        this.customerId = customerId;
+        this.rating = rating;
+        this.comments = comments;
+        this.submittedAt = submittedAt;
+    }
+
     public int getFeedbackId() { return feedbackId; }
-    public void setFeedbackId(int feedbackId) { this.feedbackId = feedbackId; }
-
     public int getTicketId() { return ticketId; }
-    public void setTicketId(int ticketId) { this.ticketId = ticketId; }
-
     public int getCustomerId() { return customerId; }
-    public void setCustomerId(int customerId) { this.customerId = customerId; }
-
-    public int getAgentId() { return agentId; }
-    public void setAgentId(int agentId) { this.agentId = agentId; }
-
     public int getRating() { return rating; }
-    public void setRating(int rating) { this.rating = rating; }
-
     public String getComments() { return comments; }
-    public void setComments(String comments) { this.comments = comments; }
-
     public Timestamp getSubmittedAt() { return submittedAt; }
-    public void setSubmittedAt(Timestamp submittedAt) { this.submittedAt = submittedAt; }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "feedbackId=" + feedbackId +
+                ", ticketId=" + ticketId +
+                ", customerId=" + customerId +
+                ", rating=" + rating +
+                ", comments='" + comments + '\'' +
+                ", submittedAt=" + submittedAt +
+                '}';
+    }
 }
--- src/model/Message.java
+++ src/model/Message.java
@@ -3,28 +3,40 @@ package model;
 import java.sql.Timestamp;
 
 public class Message {
+    public enum SenderType { AGENT, CUSTOMER }
+
     private int messageId;
     private int ticketId;
-    private String senderType;
     private int senderId;
-    private String content;
+    private SenderType senderType;
+    private String messageText;
     private Timestamp sentAt;
 
+    public Message(int messageId, int ticketId, int senderId, SenderType senderType, String messageText, Timestamp sentAt) {
+        this.messageId = messageId;
+        this.ticketId = ticketId;
+        this.senderId = senderId;
+        this.senderType = senderType;
+        this.messageText = messageText;
+        this.sentAt = sentAt;
+    }
+
     public int getMessageId() { return messageId; }
-    public void setMessageId(int messageId) { this.messageId = messageId; }
-
     public int getTicketId() { return ticketId; }
-    public void setTicketId(int ticketId) { this.ticketId = ticketId; }
-
-    public String getSenderType() { return senderType; }
-    public void setSenderType(String senderType) { this.senderType = senderType; }
-
     public int getSenderId() { return senderId; }
-    public void setSenderId(int senderId) { this.senderId = senderId; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
+    public SenderType getSenderType() { return senderType; }
+    public String getMessageText() { return messageText; }
     public Timestamp getSentAt() { return sentAt; }
-    public void setSentAt(Timestamp sentAt) { this.sentAt = sentAt; }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "messageId=" + messageId +
+                ", ticketId=" + ticketId +
+                ", senderId=" + senderId +
+                ", senderType=" + senderType +
+                ", messageText='" + messageText + '\'' +
+                ", sentAt=" + sentAt +
+                '}';
+    }
 }
